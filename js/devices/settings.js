8c629172939137a0dcbbbb4fd06a540cc3906e61
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-11-13 20:05:54 +0000

    use offset from register, not i register

--- js/devices/settings.js
+++ js/devices/settings.js
@@ -12,21 +12,21 @@ SettingsDriver.prototype._toHex = function(value) {
 	else
 		return value.toString(16)
 }
-SettingsDriver.prototype._getColor = function(chip, idx) {
+SettingsDriver.prototype._getColor = function(chip, offset) {
 	var src = chip.m
-	var offset = chip.i
-	return '#' + this._toHex(src[offset + idx]) + this._toHex(src[offset + idx + 1]) + this._toHex(src[offset + idx + 2])
+	return '#' + this._toHex(src[offset]) + this._toHex(src[offset + 1]) + this._toHex(src[offset + 2])
 }
 
 SettingsDriver.prototype.store = function(addr, value, region, chip) {
 	this.data.push(value)
 	if (this.data.length === 2) {
-		chip.backgroundColor = this._getColor(chip, 0)
-		chip.fillColor1 = this._getColor(chip, 3)
-		chip.fillColor2 = this._getColor(chip, 6)
-		chip.blendColor = this._getColor(chip, 9)
-		chip.buzzColor = this._getColor(chip, 12)
-		chip.quietColor = this._getColor(chip, 15)
+		var offset = (this.data[0] << 8) | this.data[1]
+		chip.backgroundColor = this._getColor(chip, offset + 0)
+		chip.fillColor1 = this._getColor(chip, offset + 3)
+		chip.fillColor2 = this._getColor(chip, offset + 6)
+		chip.blendColor = this._getColor(chip, offset + 9)
+		chip.buzzColor = this._getColor(chip, offset + 12)
+		chip.quietColor = this._getColor(chip,  offset + 15)
 		this.data = []
 	}
 }
