f277b7b3722a45c26ff8a288c238954287213f3d
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2017-11-13 22:03:23 +0000

    process store instruction

--- js/devices/ym2612.js
+++ js/devices/ym2612.js
@@ -3,19 +3,10 @@
 var YM2612Driver = function() {
 	this._callback = this._generate.bind(this)
 	this.ym = null
+	this.part = [[], []]
 }
 
 YM2612Driver.prototype._generate = function(e) {
-	var ctx = e.currentTarget.context
-	if (this.ym === null) {
-		console.log('creating ym2612 with sample rate ' + ctx.sampleRate)
-		var ym = new YM2612()
-		ym.init(7670448, ctx.sampleRate)
-		ym.config(9)
-		ym.reset()
-		ym.write(0x28, 0)
-		this.ym = ym
-	}
 	var dst = e.outputBuffer
 	var dstN = dst.length
 	var ymData = this.ym.update(dstN)
@@ -27,6 +18,14 @@ YM2612Driver.prototype._generate = function(e) {
 
 YM2612Driver.prototype.init = function(chip) {
 	registerAudioCallback(this._callback)
+	console.log('creating ym2612 with sample rate 44100')
+
+	var ym = new YM2612()
+	ym.init(7670448, 44100) //fixme: take sample rate from audio context
+	ym.config(9)
+	ym.reset()
+	ym.write(0x28, 0)
+	this.ym = ym
 }
 
 YM2612Driver.prototype.deinit = function(chip) {
@@ -39,6 +38,13 @@ YM2612Driver.prototype.load = function(addr, region, chip) {
 }
 
 YM2612Driver.prototype.store = function(addr, value, region, chip) {
+	var part = this.part[addr]
+	part.push(value)
+	if (part.length === 2) {
+		var reg = part.splice(0, 2)
+		//console.log('write', reg[0] + (addr? 0x100: 0), reg[1])
+		this.ym.write(reg[0] + (addr? 0x100: 0), reg[1])
+	}
 }
 
 xoioRegisterDevice(IO_ID_YM2612, function(chip) { chip.registerIODevice(IO_ID_YM2612, 2, new YM2612Driver()) })
